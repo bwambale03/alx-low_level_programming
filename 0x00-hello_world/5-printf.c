b9dde28a0b438a92700646320dc1411a909f05e5
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-08 10:01:03 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  *
  * Return: 0
  */
-int main(void){
-		
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+
+       	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

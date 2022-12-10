664eb9f34ec1fea75bcfce1d94b72e95b93f6149
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-10 03:12:32 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - a program that prints alphabets
- * in lowercase except q  and e followed 
+ * in lowercase except q and e followed 
  * by a newline
  *
  * Reurn: 0

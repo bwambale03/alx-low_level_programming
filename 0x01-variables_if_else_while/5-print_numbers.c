82c00531ea59ba3ea979ad0c97d7496757d8932b
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-10 04:03:05 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - printing singer digit numbers less than ten
+ * and starting from zero
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <=10; num++)
+	{
+		printf("%d\n", num);
+	}
+	printf("\n")
+}

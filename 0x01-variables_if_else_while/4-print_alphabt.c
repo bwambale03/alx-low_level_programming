48423157e546ea03281c7002e6c9a89c0f52471b
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-10 03:10:42 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - a program that prints alphabets
+ * in lowercase except q  and e followed 
+ * by a newline
+ *
+ * Reurn: 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != "q" && la != 'e')
+			putchar(la);
+	}
+	putchar("\n");
+	return (0);
+}
+

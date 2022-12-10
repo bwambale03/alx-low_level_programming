db40147c11e4ca494cf5d0ced4bc2c761053fcdb
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-10 01:57:10 -0800

    alphabet characters in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - a program that prints characters in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar("lc\n");
+	}
+	putchar ("\n");
+	return (0);
+}

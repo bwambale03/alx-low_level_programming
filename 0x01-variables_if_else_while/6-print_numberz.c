b5188334e71f236e869265bdb3b5212219c4a785
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-11 23:59:44 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}

320383f67708ad7f988876da06e12066ef2c665b
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-12 00:59:10 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,8 +13,8 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 		putchar((num % 10) + '0');
 
-		for (ch = 'a'; ch <= 'f'; ch++)
-			putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }

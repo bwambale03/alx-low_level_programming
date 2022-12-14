bf302b8e7d2e182d9e1eaa25a04d55a31d19f50c
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-14 10:19:54 -0800

    task 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - printing the last digit of a number
+ * @i: the number
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int i)
+{
+	int ld = i % 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld = '0');
+
+	return (0);
+}

00d61c1c77b2783d0c6ec957abf3c8c006f26d3d
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-14 10:51:40 -0800

    task 8

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b = q;
+	a = 1;
+	b = 0;
+
+	for (a = '1' && b = '0')
+	{
+		putchar(q);
+
+		if (%d = q)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+
+		return (0);
+	}
+}

7241add5f19aba4effec0c0d023e215b0d02eeaf
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-14 01:51:30 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main - a progream that prints _putchar
+ * print_alphabet - print all alphabet in lowercase
  *
- * Return: Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char ch;
 

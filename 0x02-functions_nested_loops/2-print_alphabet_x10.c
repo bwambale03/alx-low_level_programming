f059cbfdcf466a621e1d6975e7ed85b8c50dc6da
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-14 02:03:59 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing 10 times the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		if (b <= 10)
+			_putchar(b);
+	}
+	_putchar('\n');
+}

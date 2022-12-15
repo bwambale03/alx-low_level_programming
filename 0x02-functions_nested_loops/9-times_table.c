e497bfda383215ed1c2883e13185f4729e9c71ef
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-15 05:27:13 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,13 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(' ');
 		for (mult = 0; mult <= 9; mult++)
 		{
-			_putchar(',');
 			prod = num * mult;
 
 			if (prod <= 9)
 				_putchar(',');
+			_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');

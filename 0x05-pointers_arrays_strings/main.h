98af9b7d33ff40a2fa3242d77e9813171a79f72a
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-20 15:08:06 -0800

    main.h  function prototypes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _strlen(char *s);
@@ -10,7 +11,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-
 
 #endif

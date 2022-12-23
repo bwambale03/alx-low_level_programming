b15ababe5266b97b3352a53ca96997dadfd79731
Edwin Bwambale <bwambaleedwin03@gmail.com>
2022-12-23 09:34:49 -0800

    a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet- function that encodes a string into 1337
+ * @s: string to encoded
+ * Return: return the encoded string
+ */
+
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
+

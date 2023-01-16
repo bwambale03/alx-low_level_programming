56e31b31d8bada645bb75fd0fa45844de524bacc
Edwin Bwambale <bwambaleedwin03@gmail.com>
2023-01-16 08:48:12 -0800

    Defining a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "dog.h"
+/**
+ * main - defining a new data type
+ * @dog: a structure of various data types
+ * Return: 0
+ */
+int main(void)
+{
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	}
+	return (0);
+}

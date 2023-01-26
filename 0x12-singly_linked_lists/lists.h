23849c65c283c138a8e3c90636dc6d8d2d0118ad
Edwin Bwambale <bwambaleedwin03@gmail.com>
2023-01-26 13:11:47 -0800

    a function containing prototypes of function _putchar

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,28 +1,30 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Return: The number of nodes in h.
+ * Description: All prototypes used in the singly linked directory
+ * are contained here and is part of the necessities for succesfull
+ * program compilation.
  */
-
-size_t print_list(const list_t *h)
+typedef struct list_s
 {
-	size_t nodes = 0;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
-}
+#endif

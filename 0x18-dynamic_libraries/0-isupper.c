bb82f2781e0f853aaf0c6f366614514919daf40f
bwambale03 <bwambaleedwin03@gmail.com>
2023-03-21 14:39:22 -0700

    the dynamic library libdynamic.so containing various functions

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if char is upperase, otherwise 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
